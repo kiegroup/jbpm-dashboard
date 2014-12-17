ccca47fe43a5f137d7eafe89741973f064487c8e
David Gutierrez <dgutierr@redhat.com>
2014-12-17 13:16:47 +0100

    BZ-1159764 - Review SQL provider queries used in jBPM Dashboard

--- jbpm-dashboard-modules/jbpm-dashboard-webapp/src/main/java/org/jbpm/dashboard/i18n/jBPMDashboardBundleExtractor.java
+++ jbpm-dashboard-modules/jbpm-dashboard-webapp/src/main/java/org/jbpm/dashboard/i18n/jBPMDashboardBundleExtractor.java
@@ -35,14 +35,14 @@ public class jBPMDashboardBundleExtractor {
         // Process the Showcase KPIs file
         XmlToBundleConverter converter = new KpisFileConverter();
         converter.bundleDir = new File(rootDir, "src/main/resources/org/jbpm/dashboard/kpis");
-        converter.xmlFile = new File(rootDir, "src/main/webapp/WEB-INF/deployments/jbpmKPIs.kpis");
+        converter.xmlFile = new File(rootDir, "src/main/webapp/WEB-INF/deployments/jbpmKPIs_v2.kpis");
         Map<Locale,Properties> bundles = converter.extract(); // Extract bundles from source XML
         converter.write(bundles); // Write literals to bundle files.
 
         // Process the Showcase Workspace file
         converter = new WorkspaceFileConverter();
         converter.bundleDir = new File(rootDir, "src/main/resources/org/jbpm/dashboard/workspace");
-        converter.xmlFile = new File(rootDir, "src/main/webapp/WEB-INF/deployments/jbpmWorkspace.workspace");
+        converter.xmlFile = new File(rootDir, "src/main/webapp/WEB-INF/deployments/jbpmWorkspace_v2.workspace");
         bundles = converter.extract(); // Extract bundles from source XML
         converter.write(bundles); // Write literals to bundle files.
     }
